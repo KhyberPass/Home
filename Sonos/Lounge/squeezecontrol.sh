badd6e8a0902a553c296d9600f45004a7cf65ccd
KhyberPass <KhyberPass@users.noreply.github.com>
2018-05-23 10:45:00 +1000

    Add a timer so amp is only turned off when paused for some time

--- Sonos/Lounge/squeezecontrol.sh
+++ Sonos/Lounge/squeezecontrol.sh
@@ -5,6 +5,11 @@ echo 17  > /sys/class/gpio/export
 echo out > /sys/class/gpio/gpio17/direction
 echo 0 > /sys/class/gpio/gpio17/value
 
+# Init the timer and state
+timerlast=$(date '+%s')
+timerdiff=0
+state=0
+
 # Read a line from stdio, this is a blocking call
 while read line
 do
@@ -14,17 +19,28 @@ do
         #echo "Test " ${linearr[3]}
 
         if [ "${linearr[3]}" = 'command' ] && [ "${linearr[4]}" = 's' ]; then
+                timerlast=$(date '+%s')
+                state=1
                 echo 1 > /sys/class/gpio/gpio17/value
-                echo "Start"
+                #echo "Start"
         fi
 
         if [ "${linearr[3]}" = 'command' ] && [ "${linearr[4]}" = 'u' ]; then
+                timerlast=$(date '+%s')
+                state=1
                 echo 1 > /sys/class/gpio/gpio17/value
-                echo "Unpause"
+                #echo "Unpause"
         fi
 
-        if [ "${linearr[3]}" = 'command' ] && [ "${linearr[4]}" = 'p' ]; then
+        timercurr=$(date '+%s')
+        timerdiff=$(($timercurr - $timerlast))
+        if [ $state -eq 1 ] && [ $timerdiff -gt 600 ]; then
+                state=0
                 echo 0 > /sys/class/gpio/gpio17/value
-                echo "Pause"
+                #echo "Off"
         fi
+
 done
+
+# Remove the gpio setup when exiting
+echo 17  > /sys/class/gpio/unexport
